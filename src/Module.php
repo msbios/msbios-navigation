e04b8c3522b16d7327a8b6379d62985faf1ccbca
Judzhin Miles <info@msbios.com>
2019-04-22 16:30:34 +0300

    Lazy Composer MSBios Commit Script

--- src/Module.php
+++ src/Module.php
@@ -12,7 +12,7 @@ namespace MSBios\Navigation;
 class Module extends \MSBios\Module
 {
     /** @const VERSION */
-    const VERSION = '1.0.1';
+    const VERSION = '1.0.2';
 
     /**
      * @inheritdoc
