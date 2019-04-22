d7afd2c738eb32dc7d2bfe84b32a47daee34ba88
Judzhin Miles <info@msbios.com>
2019-04-22 16:28:08 +0300

    Lazy Composer MSBios Commit Script

--- config/module.config.php
+++ config/module.config.php
@@ -3,29 +3,31 @@
  * @access protected
  * @author Judzhin Miles <info[woof-woof]msbios.com>
  */
+
 namespace MSBios\Navigation;
 
 return [
     'navigation' => [
         'default' => [
-            'home' => [
-                'label' => 'Home',
-                'route' => 'home',
-            ],
-            'page-1' => [
-                'label' => 'Page #1',
-                'uri' => '#page1',
-            ],
-            'page-2' => [
-                'label' => 'Page #2',
-                'uri' => '#page2',
-            ],
+            // 'home' => [
+            //     'label' => 'Home',
+            //     'route' => 'home',
+            // ],
+            // 'page-1' => [
+            //     'label' => 'Page #1',
+            //     'uri' => '#page1',
+            // ],
+            // 'page-2' => [
+            //     'label' => 'Page #2',
+            //     'uri' => '#page2',
+            // ],
         ],
     ],
 
     'service_manager' => [
         'factories' => [
-            'navigation' => \Zend\Navigation\Service\DefaultNavigationFactory::class,
+            'navigation' =>
+                \Zend\Navigation\Service\DefaultNavigationFactory::class,
         ],
     ],
 ];
